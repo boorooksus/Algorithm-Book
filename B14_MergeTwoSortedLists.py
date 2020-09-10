efc6e0ef8504646d2e1eecbdb37cd7082397536b
boorooksus <boorooksus@gmail.com>
2020-09-11 00:54:06 +0900

    오류 코드

--- B14_MergeTwoSortedLists.py
+++ B14_MergeTwoSortedLists.py
@@ -6,4 +6,23 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        nums = []
+        while l1.next is not None:
+            nums.append(l1.val)
+            l1 = l1.next
+        nums.append(l1.val)
 
+        while l2.next is not None:
+            nums.append(l2.val)
+            l2 = l2.next
+        nums.append(l2.val)
+
+        nums.sort()
+        node = ListNode()
+        node.val = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            node.next = node
+            node.val = nums[i]
+        return node
+
+# 오류
