771f78cf770655dfc1749cff8cf4a29640b81e0b
boorooksus <boorooksus@gmail.com>
2020-09-12 18:15:08 +0900

    Create BP12_BestTimeToBuyNSellStock.py

---
+++ Practice/BP12_BestTimeToBuyNSellStock.py
@@ -0,0 +1,14 @@
+from typing import List
+import sys
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit, min_price = 0, sys.maxsize
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
+        return max_profit
+
+
+
