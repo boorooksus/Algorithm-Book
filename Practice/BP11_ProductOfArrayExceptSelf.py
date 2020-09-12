4921a62ce92a4f8fbb1414f7d509bbc33f94da77
boorooksus <boorooksus@gmail.com>
2020-09-12 20:38:44 +0900

    다시 풀 것!

---
+++ Practice/BP11_ProductOfArrayExceptSelf.py
@@ -0,0 +1,19 @@
+from typing import List
+import functools
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        p = 1
+        res = []
+        for i in range(len(nums)):
+            res.append(p)
+            p *= nums[i]
+        p = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= p
+            p *= nums[i]
+
+        return res
+    
+# 다시 풀것!!
