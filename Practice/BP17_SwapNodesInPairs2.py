0c03ef543f7d27f60cdea7b1df761474959c794e
boorooksus <boorooksus@gmail.com>
2020-10-10 20:58:56 +0900

    재귀 이용

---
+++ Practice/BP17_SwapNodesInPairs2.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head and head.next:
+            temp = head.next
+            head.next = self.swapPairs(temp.next)
+            temp.next = head
+            return temp
+        return head
