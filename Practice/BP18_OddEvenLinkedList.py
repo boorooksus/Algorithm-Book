a230ab492f4a110c9c5d1f55e803e179a1fa9edc
boorooksus <boorooksus@gmail.com>
2020-10-11 15:51:45 +0900

    내가 쓴 풀이

---
+++ Practice/BP18_OddEvenLinkedList.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        even_root = even = ListNode(None)
+        prev = ListNode(None)
+        prev.next = head
+        root = head
+
+        while head and head.next:
+            even.next = head.next
+            head.next, prev.next = head.next.next, head
+            head, even, prev = head.next, even.next, prev.next
+        if prev.next and even_root.next:
+            even.next = None
+            head.next = even_root.next
+        elif prev.next is None and even_root.next:
+            even.next = None
+            prev.next = even_root.next
+        return root
+
+
+
+
+
+sol = Solution()
+root = head = ListNode(None)
+for i in range(1, 6):
+    head.next = ListNode(i)
+    head = head.next
+sol.oddEvenList(root.next)
