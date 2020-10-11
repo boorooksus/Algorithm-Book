992d33c85dc1bc00ab4f586abebf4775c27205ae
boorooksus <boorooksus@gmail.com>
2020-10-11 17:06:35 +0900

    책 풀이

---
+++ Practice/BP18_OddEvenLinkedList2.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        odd = head
+        even = head.next
+        even_root = head.next
+
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = even_root
+        return head
+
+# leetcode: 328
+#
