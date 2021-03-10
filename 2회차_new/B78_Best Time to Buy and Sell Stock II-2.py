047b77b28d61079ae1be07e00bdf98a3e600bebf
boorooksus <boorooksus@gmail.com>
2021-03-10 14:13:08 +0900

    그리디 알고리즘

---
+++ 2회차_new/B78_Best Time to Buy and Sell Stock II-2.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(max(0, prices[i + 1] - prices[i]) for i in range(len(prices) - 1))
+    
