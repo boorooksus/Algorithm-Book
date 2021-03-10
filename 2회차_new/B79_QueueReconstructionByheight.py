ff7a109ac0aa8307d2b85f8e5f29d94747170592
boorooksus <boorooksus@gmail.com>
2021-03-10 11:02:24 +0900

    우선순위 큐 활용

---
+++ 2회차_new/B79_QueueReconstructionByheight.py
@@ -0,0 +1,16 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        hq = []
+        for person in people:
+            heapq.heappush(hq, [-person[0], person[1]])
+            
+        res = []
+        while hq:
+            person = heapq.heappop(hq)
+            res.insert(person[1], [-person[1], person[1]])
+
+        return res
