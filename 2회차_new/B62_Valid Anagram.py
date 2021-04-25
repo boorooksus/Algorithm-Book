262171c5e6f66228cf2e3855ee54385fcd938029
boorooksus <boorooksus@gmail.com>
2021-04-25 09:26:19 +0900

    anagram

---
+++ 2회차_new/B62_Valid Anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = sorted(list(s))
+        b = sorted(list(t))
+        return True if a == b else False
+
+
+print(Solution().isAnagram("rat", "car"))
