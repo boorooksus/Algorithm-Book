017e106123b6efd59ae9e8c79e198875b97d802d
boorooksus <boorooksus@gmail.com>
2021-07-23 14:13:29 +0900

    가스 스테이션 방문 - O(n)

---
+++ 3회차/B81_Gas Station2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        fuel, start = 0, 0
+        for i in range(len(gas)):
+            if fuel + gas[i] < cost[i]:
+                start = i + 1
+                fuel = 0
+            else:
+                fuel = fuel + gas[i] - cost[i]
+        return start
+
+
+print(Solution().canCompleteCircuit([1,2,3,4,5],
+[3,4,5,1,2]))
