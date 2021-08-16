2f71d4500a357ca7526bf9cd10eb19766e3aece7
boorooksus <boorooksus@gmail.com>
2021-08-16 09:53:38 +0900

    BST 중위 탐색 - 재귀

---
+++ 3회차/B51_Binary Search Tree to Greater Sum Tree2.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    val: int = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+
+        return root
+
+"""
+중위순회 거꾸로
+"""
