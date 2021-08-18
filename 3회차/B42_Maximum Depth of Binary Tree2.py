ba5ace69c1cd1acc9a4970165662d39b1aadf52f
boorooksus <boorooksus@gmail.com>
2021-08-18 11:43:45 +0900

    이진 트리 깊이 - BFS

---
+++ 3회차/B42_Maximum Depth of Binary Tree2.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        result = 0
+        dq = deque([root])
+        while dq:
+            result += 1
+            for _ in range(len(dq)):
+                node = dq.popleft()
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+
+        return result
