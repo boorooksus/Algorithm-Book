0ef885fcf137e0fe95efadc6d2cf9dfc333321a1
boorooksus <boorooksus@gmail.com>
2021-09-03 13:43:08 +0900

    오름차순 짝수번째 값 계산

--- 3회차/B10_Array Partition I.py
+++ 3회차/B10_Array Partition I.py
@@ -3,3 +3,9 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        result = 0
+        for i in range(0, len(nums), 2):
+            result += nums[i]
+        return result
+
