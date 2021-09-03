c354a747df5c9662fc4318728f49b92a393bbb8a
boorooksus <boorooksus@gmail.com>
2021-09-03 14:15:18 +0900

    브루트 포스 - 시간 초과

---
+++ 3회차/B09_3Sum.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+
+        result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[k] == nums[k - 1]:
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        result.append([nums[i], nums[j], nums[k]])
+
+        return result
+
