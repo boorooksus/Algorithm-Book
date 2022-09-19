98e99cd60e535d391863c5b4281ee8ea84d6489b
Boo Sung Jun <boorooksus@gmail.com>
2022-09-19 15:22:03 +0900

    다이나믹 프로그래밍

---
+++ 백준/2회차/S1-15724-JuJiSu.py
@@ -0,0 +1,17 @@
+from sys import stdin
+input = lambda: stdin.readline().rstrip()
+
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    arr = [[0] * (M + 1)] + list([0] + list(map(int, input().split())) for _ in range(N))
+
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1]
+
+    K = int(input())
+    for _ in range(K):
+        y1, x1, y2, x2 = map(int, input().split())
+        print(arr[y2][x2] - arr[y1 - 1][x2] - arr[y2][x1 - 1] + arr[y1 - 1][x1 - 1])
+
